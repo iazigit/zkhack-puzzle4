9609fb6f761e100b2f6abc9b4aac5279521287c3
iazigit <31214223+iazigit@users.noreply.github.com>
2021-11-17 01:48:23 +0100

    Soulved

--- src/bin/verify-hidden-in-plain-sight.rs
+++ src/bin/verify-hidden-in-plain-sight.rs
@@ -7,7 +7,7 @@ use ark_poly::{
     UVPolynomial,
 };
 use ark_serialize::CanonicalDeserialize;
-use hidden_in_plain_sight::{generate::kzg_commit, PUZZLE_DESCRIPTION};
+use hidden_in_plain_sight::{generate::*, PUZZLE_DESCRIPTION};
 use prompt::{puzzle, welcome};
 
 fn read_cha_from_file() -> (Vec<G1Affine>, Vec<Vec<Fr>>, Fr, Fr, G1Affine, Fr, Fr) {
@@ -43,8 +43,42 @@ fn main() {
 
     let (setup, accts, cha_1, cha_2, commt, opn_1, opn_2) = read_cha_from_file();
 
+
+    let domain: GeneralEvaluationDomain<Fr> =
+        GeneralEvaluationDomain::new(1002).unwrap();
+
+    let v1 = domain.vanishing_polynomial().evaluate(&cha_1);
+    let v2 = domain.vanishing_polynomial().evaluate(&cha_2);
+
+    let solution_blinded_acct = {
+        
+        // Account found by bruteforce
+        let acct = &accts[535];
+
+        let acct_poly = DensePolynomial::from_coefficients_vec(domain.ifft(&acct));
+
+        // Recovering blinding polynomial by interpolation
+        let p1 = acct_poly.evaluate(&cha_1);
+        let tmp1 = (opn_1 - p1) * v1.inverse().unwrap();
+
+        let p2 = acct_poly.evaluate(&cha_2);
+        let tmp2 = (opn_2 - p2) / v2;
+
+        let b1 = (tmp1 - tmp2)/(cha_1 - cha_2);
+        let b0 = tmp1 - cha_1 * b1;
+        let recovd_b_poly = DensePolynomial::from_coefficients_vec(vec![b0, b1]);
+        
+        let recovd_ba_poly = recovd_b_poly.mul_by_vanishing_poly(domain) + acct_poly;
+        let r_commit : G1Affine = kzg_commit(&recovd_ba_poly, &setup);
+         
+        //println!("{:?}\n{:?}\n", r_commit, commt);
+        //if r_commit == commt {
+            println!("Soulved!");
+            recovd_ba_poly
+        //}
+    };
+
     // Replace with the solution polynomial, derived from the account!
-    let solution_blinded_acct = DensePolynomial::from_coefficients_vec(vec![]);
 
     let solution_commitment = kzg_commit(&solution_blinded_acct, &setup);
     assert_eq!(solution_commitment, commt);
--- src/generate.rs
+++ src/generate.rs
@@ -47,7 +47,7 @@ fn generate_accts(n: usize) -> Vec<Vec<Fr>> {
         .collect::<Vec<_>>()
 }
 
-fn generate_challenge() -> (Vec<G1Affine>, Vec<Vec<Fr>>, Fr, Fr, G1Affine, Fr, Fr) {
+pub fn generate_challenge() -> (Vec<G1Affine>, Vec<Vec<Fr>>, Fr, Fr, G1Affine, Fr, Fr) {
     use rand::Rng;
     let mut rng = rand::thread_rng();
 
@@ -56,14 +56,49 @@ fn generate_challenge() -> (Vec<G1Affine>, Vec<Vec<Fr>>, Fr, Fr, G1Affine, Fr, F
     let target_acct_index = rng.gen_range(0..number_of_accts);
     let target_acct = &accts[target_acct_index];
 
+    println!("acct_{} ({}) = {:?}", target_acct_index, target_acct.len(), target_acct);
+
     let domain: GeneralEvaluationDomain<Fr> =
         GeneralEvaluationDomain::new(number_of_accts + 2).unwrap();
     let setup = generate_kzg_setup(domain.size());
 
+    println!("setup {:?} ({})", setup, setup.len());
+    println!("num_accounts = {}", number_of_accts);
+
     let target_acct_poly = DensePolynomial::from_coefficients_vec(domain.ifft(&target_acct));
     let blinding_poly =
         DensePolynomial::from_coefficients_vec(vec![Fr::rand(&mut rng), Fr::rand(&mut rng)]);
-    let blinded_acct_poly = target_acct_poly + blinding_poly.mul_by_vanishing_poly(domain);
+    let blinded_acct_poly = target_acct_poly.clone() + blinding_poly.mul_by_vanishing_poly(domain);
+
+    let vanish =  DensePolynomial::from_coefficients_vec(domain.ifft(&vec![Fr::from(0); domain.size()]));//.mul_by_vanishing_poly(domain);
+
+
+    let domain2: GeneralEvaluationDomain<Fr> =
+        GeneralEvaluationDomain::new(number_of_accts * 2).unwrap();
+
+    let blinded_evals = blinded_acct_poly.clone().evaluate_over_domain(domain).evals;
+    let target_evals = target_acct_poly.clone().evaluate_over_domain(domain).evals;
+    let blinding_evals = blinding_poly.mul_by_vanishing_poly(domain).evaluate_over_domain(domain).evals;
+
+
+    let tmp_poly = blinded_acct_poly.divide_by_vanishing_poly(domain).unwrap().0;
+    assert_eq!(tmp_poly, blinding_poly);
+    
+
+    for (i,(e, t)) in target_evals.iter().zip(target_acct).enumerate() {
+        println!("{} {:?}\n  {:?}", i, e, t);
+    }
+
+    for (i,e) in blinding_evals.iter().enumerate() {
+        println!("{} {:?}", i, e);
+    }
+
+    println!("{} {} {} {}", target_acct_poly.degree(), blinding_poly.degree(), blinded_acct_poly.degree(), &vanish.degree());
+
+
+    for acct in accts.clone() {
+    //    println!("{:?}", acct);
+    }
 
     let commitment: G1Affine = kzg_commit(&blinded_acct_poly, &setup);
 
@@ -72,6 +107,25 @@ fn generate_challenge() -> (Vec<G1Affine>, Vec<Vec<Fr>>, Fr, Fr, G1Affine, Fr, F
 
     let opening_1 = blinded_acct_poly.evaluate(&challenge_1);
     let opening_2 = blinded_acct_poly.evaluate(&challenge_2);
+
+    let p1 = target_acct_poly.evaluate(&challenge_1);
+    let v1 = domain.vanishing_polynomial().evaluate(&challenge_1);
+    let tmp1 = (opening_1 - p1) * v1.inverse().unwrap();
+
+    let p2 = target_acct_poly.evaluate(&challenge_2);
+    let v2 = domain.vanishing_polynomial().evaluate(&challenge_2);
+    let tmp2 = (opening_2 - p2) / v2;
+    //println!("{:?}", tmp2);
+    //println!("{:?}", blinding_poly.evaluate(&challenge_2));
+
+    let b1 = (tmp1 - tmp2)/(challenge_1 - challenge_2);
+    let b0 = tmp1 - challenge_1 * b1;
+    println!("{:?}", b1);
+    println!("{:?}", blinding_poly.coeffs[1]);
+    let recovd_b_poly = DensePolynomial::from_coefficients_vec(vec![b0, b1]);
+    assert_eq!(recovd_b_poly, blinding_poly);
+    println!("eq!");
+
 
     (
         setup,
